7fd4831cb9e0033979205532a312efeedb3fda3c
Artur Romanchev <aromanchev@businessolver.com>
2023-05-14 20:01:43 +0400

    init project :tada:

---
+++ bot.py
@@ -0,0 +1,55 @@
+import prettytable
+import telebot
+import constants
+import os
+import time
+import api_requests
+
+TOKEN = os.environ["TOKEN"]
+bot = telebot.TeleBot(TOKEN, parse_mode="HTML")
+
+def calculate_spread(sending_amount, rate, binance_rate):
+    receiving_amount = constants.KORONA_RECEIVING_AMOUNT / 100
+    total_usd = receiving_amount / rate
+    binance_total_amount = total_usd * binance_rate
+    print(binance_total_amount)
+    spread = 100 - (sending_amount / binance_total_amount * 100)
+    return round(spread, 2)
+
+def create_table():
+    exchangers = api_requests.get_exchangers()
+
+    binance_p2p_rate = float(api_requests.get_binance_p2p_rate())
+    korona_pay = api_requests.get_koronapay()
+    korona_pay_exchange_rate = korona_pay[0]['exchangeRate']
+    korona_pay_sending_amount = korona_pay[0]['sendingAmount'] / 100
+
+    table = prettytable.PrettyTable(['Обменник', 'USD' ,'Спред'])
+    table.align['Обменник'] = 'l'
+    table.align['USD'] = 'l'
+    table.align['Спред'] = 'l'
+    table.border = False
+
+    for exchanger in exchangers:
+        rate = exchanger['rates']['sellRate']
+        company = exchanger['company']
+        spread =  calculate_spread(korona_pay_sending_amount, rate, binance_p2p_rate)
+        table.add_row([company, f'{rate:.3f}', f'{spread}%'])
+
+    table.add_row(['------------', '', ''])
+    table.add_row([f'\nKoronaPay GEL: ', f'\n{korona_pay_exchange_rate:.3f}', ''])
+    
+    content = f'<pre>{table}</pre>'
+    return content
+
+@bot.message_handler(commands=['start'])
+def start_bot(message):
+    table = create_table()
+
+    bot.send_message(message.chat.id, f'Добро пожаловать {message.from_user.first_name}!')
+    bot.send_message(message.chat.id, table)
+    
+    while(True):
+        bot.send_message(message.chat.id, table, time.sleep(60))
+	
+bot.polling()
---
+++ constants.py
@@ -0,0 +1,28 @@
+EXCHANGE_RATE_API = 'https://mapi.ge/_next/data/Id9M_PzvWg56xBII11XLF/index.json'
+
+KORONA_RECEIVING_AMOUNT = 400000
+KORONA_API = 'https://koronapay.com/transfers/online/api/transfers/tariffs'
+KORONA_API_QUERY = {
+    'sendingCountryId': 'RUS',
+    'sendingCurrencyId': '810',
+    'receivingCountryId': 'GEO',
+    'receivingCurrencyId': 981,
+    'paymentMethod': 'debitCard',
+    'receivingAmount': 400000,
+    'receivingMethod': 'cash',
+    'paidNotificationEnabled': False
+}
+KORONA_API_HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
+
+BINANCE_API = 'https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search'
+BINANCE_API_PAYLOAD = {
+  'asset': 'USDT',
+  'fiat': 'RUB',
+  'countries': ['RU'],
+  'merchantCheck': False,
+  'page': 1,
+  'payTypes': ['TinkoffNew', 'RaiffeisenBank'],
+  'publisherType': None,
+  'rows': 10,
+  'tradeType': 'sell'
+}
