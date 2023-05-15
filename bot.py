689e0d216e5a6b9d9b0da19674366dc00c110ef1
Artur Romanchev <aromanchev@businessolver.com>
2023-05-15 14:50:32 +0400

    update bot

--- bot.py
+++ bot.py
@@ -42,10 +42,6 @@ def create_table():
     content = f'<pre>{table}</pre>'
     return content
 
-@bot.message_handler(commands=['lol'])
-def lol(message):
-    bot.send_message(message.chat.id, f'Добро пожаловать @{message.from_user.username}!')
-
 @bot.message_handler(commands=['start'])
 def start_bot(message):
     bot.send_message(message.chat.id, f'Добро пожаловать @{message.from_user.username}!')
