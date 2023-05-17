38627050ecea7d5d0c3429536aff8236a852a85b
Artur Romanchev <aromanchev@businessolver.com>
2023-05-18 02:14:51 +0400

    update bot

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import asyncio
 import prettytable
-from telebot.async_telebot import AsyncTeleBot
+import telebot
 import constants
 import os
 import api_requests
 
 TOKEN = os.environ["TOKEN"]
-bot = AsyncTeleBot(TOKEN, parse_mode="HTML")
+bot = telebot.async_telebot.AsyncTeleBot(TOKEN, parse_mode="HTML")
 
 
 def calculate_spread(sending_amount: int, rate: int, binance_rate: int):
@@ -57,5 +57,5 @@ async def start_bot(message):
 while True:
     try:
         asyncio.run(bot.polling(none_stop=True, timeout=9999999, interval=5))
-    except Exception as error:
+    except (telebot.apihelper.ApiException, Exception) as error:
         print(error)
