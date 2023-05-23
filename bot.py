c9fd44ba54a26ee3974862766ad58e83843e4dc9
Artur Romanchev <aromanchev@businessolver.com>
2023-05-24 00:01:20 +0400

    update bot

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from telegram.constants import ParseMode
 
 # ---------------------------- B0T CONFIG ------------------------------
 TOKEN = os.environ["TOKEN"]
-application = Application.builder().token(TOKEN).read_timeout(60).get_updates_read_timeout(60).build()
+application = Application.builder().token(TOKEN).read_timeout(7).get_updates_read_timeout(42).build()
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -82,7 +82,7 @@ def create_table():
     return content
 
 # ---------------------------- BOT ACTIONS ------------------------------
-async def start_bot(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_chat.id
     table = create_table()
     await context.bot.send_message(chat_id=chat_id, text=f'Добро пожаловать @{update.message.from_user.username}!')
@@ -101,6 +101,14 @@ async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> N
 
     await context.bot.send_message(chat_id=update.effective_chat.id, text='Ошибка:(', parse_mode=ParseMode.HTML)
 
-application.add_handler(CommandHandler("start", start_bot))
-application.add_error_handler(error_handler)
-application.run_polling()
+def main() -> None:
+    application = Application.builder().token("TOKEN").build()
+
+    application.add_error_handler(error_handler)
+    application.add_handler(CommandHandler(["start"], start))
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
