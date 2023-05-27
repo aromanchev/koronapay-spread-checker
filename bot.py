f8b1dcd6b26dffaf7158f704cde36978d8492353
Artur Romanchev <aromanchev@businessolver.com>
2023-05-27 15:04:16 +0400

    update bot

--- bot.py
+++ bot.py
@@ -121,6 +121,6 @@ application.add_handler(CommandHandler("stop_messaging", stop_messaging))
 application.run_webhook(
         listen="0.0.0.0",
         port=int(PORT),
-        secret_token="123ef55338c8c30a61d2aae399c1cb8502d6c3b2c3d7143f01d7be5e4bf6",
-        webhook_url='https://koronapay-spread.herokuapp.com/'
+        # secret_token="123ef55338c8c30a61d2aae399c1cb8502d6c3b2c3d7143f01d7be5e4bf6",
+        webhook_url='https://koronapay-spread.herokuapp.com/123ef55338c8c30a61d2aae399c1cb8502d6c3b2c3d7143f01d7be5e4bf6'
     )
